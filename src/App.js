4efb8d30fd18413f0d81462ce5d2f5bec985d80d
OksanaKis <oksanakisetoja@gmail.com>
2021-11-09 12:39:52 +0200

    adding props ingredients

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   useEffect(() => {
     console.log('Effect has been run');
     getRecipes()
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
@@ -31,6 +31,7 @@ function App() {
   const getSearch = (event) => {
     event.preventDefault();
     setQuery(search);
+    setSearch('');
   }
 
   return (
@@ -38,7 +39,7 @@ function App() {
       <div><h1>FIND YOUR RECIPE!</h1></div>
       <form onSubmit={getSearch} className="search-form">
         <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
-        <button className="search-button" type="submit"></button>
+        <button className="search-button" type="submit">Search</button>
       </form>
       {recipes.map(recipe => (
         <Recipe 
@@ -46,6 +47,7 @@ function App() {
         title={recipe.recipe.label}
         calories={recipe.recipe.calories}
         image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}
         />
       ))}
     </div>
